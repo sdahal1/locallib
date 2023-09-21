40f1275dc5f951515b2f7691bbf6692b7bd9c20d
sdahal1 <raabdahl@gmail.com>
2023-09-21 11:28:17 -0700

    Complete qualified assignment, ...

---
+++ public/src/home.js
@@ -0,0 +1,22 @@
+function getTotalBooksCount(books=[]) {
+  return books.length;
+}
+
+function getTotalAccountsCount(accounts) {}
+
+function getBooksBorrowedCount(books) {}
+
+function getMostCommonGenres(books) {}
+
+function getMostPopularBooks(books) {}
+
+function getMostPopularAuthors(books, authors) {}
+
+module.exports = {
+  getTotalBooksCount,
+  getTotalAccountsCount,
+  getBooksBorrowedCount,
+  getMostCommonGenres,
+  getMostPopularBooks,
+  getMostPopularAuthors,
+};
